42d7e9e26d490df845b150169b66838bc48ab658
Enoch Otieno <enochotieno@rocketmail.com>
2023-02-06 19:01:47 +0300

    Add student calculation program

---
+++ student_marks.py
@@ -0,0 +1,17 @@
+'''Create student details'''
+
+print("Enter the student names:\n")
+name = input()
+print("Enter the student class:\n")
+stream = input()
+print("Enter the marks maths:\n")
+maths = input()
+print("Enter the marks english:\n")
+english = input()
+print("Enter the marks science:\n")
+science = input()
+print("---------------------\n")
+total = int(maths) + int(science) + int(english)
+mean = int(total)/3
+print("|\t Student\t English\t Maths\t Science\t Total\t Mean\t ")
+print("|" + name + "\t" + str(english) + "\t" + str(maths) + "\t" + str(science) + "\t" + str(total) + "\t" + str(mean) + "|")

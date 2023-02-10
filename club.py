59e856eab3a8fe199ff161cd0d40856a1711b2c5
Enoch Otieno <enochotieno@rocketmail.com>
2023-02-10 18:25:52 +0300

    Added while loop

---
+++ club.py
@@ -0,0 +1,9 @@
+age = input("Enter the age of individual: ")
+age_limit = 21
+if int(age) < int(age_limit):
+    print("No entry!")
+else:
+    print("Please proceed")
+    
+
+    
--- loops.py
+++ loops.py
@@ -1,9 +1,16 @@
 ''' For Loop'''
 # Loops allow us to reiterate a process over and over until a point where we it to stop
 
-prime_numbers = [2,3,5,7,9,11,13,15]
+# prime_numbers = [2,3,5,7,9,11,13,15]
 
-for prime in prime_numbers:
-    print(prime)
+# for prime in prime_numbers:
+#    print(prime)
 
 '''While loop'''
+
+count = 10
+i = 0
+while i < count:
+    i+=1
+    print(i)
+
